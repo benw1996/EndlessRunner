110f9f52c60aeff2188bb9b121abea0ef5e3cdea
Ben Warren <33360056+benw1996@users.noreply.github.com>
2018-02-20 14:56:25 +0000

    Added script to instantiate prefab next to the segment.
    Currently just places the next segment in the static location.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public List<GameObject> levelSegments;
+    private int currentSegment = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public GameObject GetNextSegment() {
+        if (currentSegment != levelSegments.Count - 1) {
+            currentSegment++;
+        } else {
+            currentSegment = 0;
+        }
+
+        return levelSegments[currentSegment];
+    }
+}
--- Assets/Scripts/LevelComponent.cs
+++ Assets/Scripts/LevelComponent.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class LevelComponent : MonoBehaviour {
 
+    //Transform of child object with location for the next component
+    private Transform nextLocation;
+
+    private GameController gameController;
+
 	// Use this for initialization
 	void Start () {
-		
+        nextLocation = gameObject.transform.GetChild(0);
+
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+
+        DisplayNextSegment();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    /// <summary>
+    /// Method for creating the next level component.
+    /// </summary>
+    void DisplayNextSegment() {
+        GameObject nextSegment = gameController.GetNextSegment();
+
+        Instantiate(nextSegment, nextLocation.position, nextLocation.rotation);
+    }
 }
