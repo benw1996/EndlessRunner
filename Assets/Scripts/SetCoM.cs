548891d035288902b0d5fdb74c95d89dbe7373bb
Ben Warren <33360056+benw1996@users.noreply.github.com>
2018-05-01 17:04:53 +0100

    Added in a script to set the center of mass for an object.
    Script takes in the transform of the position for the desired center of mass then sets the objects center to the vector of the transform.

---
+++ Assets/Scripts/SetCoM.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCoM : MonoBehaviour {
+
+    public Transform com;
+    private Vector2 m_com;
+    private Rigidbody2D m_rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        m_rigidbody = GetComponent<Rigidbody2D>();
+        m_com = com.position;
+
+        m_rigidbody.centerOfMass = m_com;
+	}
+}
